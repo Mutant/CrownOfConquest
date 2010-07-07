ba61856a3b8ec1174430245f6ccf393930e2ae7d
sam <sam@945459e7-7517-47ec-917d-8f43ad2a08c9>
2010-07-07 07:57:34 +0000

    Fixed script

--- script/error_summary.pl
+++ script/error_summary.pl
@@ -1,13 +1,16 @@
+#!/usr/bin/perl
+
 use strict;
 use warnings;
 
 use MIME::Lite;
+use YAML;
 
 my $home = $ENV{RPG_HOME};
 
 my $config = YAML::LoadFile("$home/rpg.yml");
 
-my $errors = `grep '\[error\]' $home/log/debug.log.1`;
+my $errors = `grep '\\[error\\]' $home/log/debug.log.1`;
 
 my $msg = MIME::Lite->new(
 	From    => 'mutant.nz@gmail.com',

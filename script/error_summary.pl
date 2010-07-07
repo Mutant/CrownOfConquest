110482e55cbc6e75cd7647e1a45f1ef4fe79be5e
sam <sam@945459e7-7517-47ec-917d-8f43ad2a08c9>
2010-07-07 07:51:13 +0000

    Added script to summarise errors each day and send an email

---
+++ script/error_summary.pl
@@ -0,0 +1,18 @@
+use strict;
+use warnings;
+
+use MIME::Lite;
+
+my $home = $ENV{RPG_HOME};
+
+my $config = YAML::LoadFile("$home/rpg.yml");
+
+my $errors = `grep '\[error\]' $home/log/debug.log.1`;
+
+my $msg = MIME::Lite->new(
+	From    => 'mutant.nz@gmail.com',
+	To      => 'mutant.nz@gmail.com',
+	Subject => '[Kingdoms] Daily error summary',
+	Data    => $errors,
+);
+$msg->send( 'smtp', $config->{smtp_server}, Debug => 0, );

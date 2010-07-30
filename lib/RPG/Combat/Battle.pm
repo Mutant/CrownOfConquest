e764c5345607f9a394d73fbe5d3c304351d1d0a0
sam <sam@945459e7-7517-47ec-917d-8f43ad2a08c9>
2010-07-30 22:49:57 +0000

    Clean up combat params for each char after battle

--- lib/RPG/Combat/Battle.pm
+++ lib/RPG/Combat/Battle.pm
@@ -766,9 +766,11 @@ sub end_of_combat_cleanup {
 		# Remove last_combat_actions that can't be carried between combats
 		#  (currently just 'cast')
 		if ( $combatant->last_combat_action eq 'Cast' ) {
-			$combatant->last_combat_action('Defend');
-			$combatant->update;
+			$combatant->last_combat_action('Defend');			
 		}
+		$combatant->last_combat_param1(undef);
+		$combatant->last_combat_param2(undef);
+		$combatant->update;
 	}
 }
 

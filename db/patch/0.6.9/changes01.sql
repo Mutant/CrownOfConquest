6b76f50e6812454ac6a7e3770327964f80f565ba
Sam Crawley <mutant.nz@gmail.com>
2010-12-05 10:51:56 +1300

    New monster portraits

--- db/patch/0.6.9/changes01.sql
+++ db/patch/0.6.9/changes01.sql
@@ -1,1 +1,9 @@
 update Land set creature_threat = 0;
+
+update Creature_Type set image = 'wispsmall.png' where creature_type = 'Wisp';
+update Creature_Type set image = 'wyvernsmall.png' where creature_type = 'Wyvern';
+update Creature_Type set image = 'orcgruntsmall.png' where creature_type = 'Orc Grunt';
+update Creature_Type set image = 'hobgoblinsmall.png' where creature_type = 'Hobgoblin';
+update Creature_Type set image = 'firedragonsmall.png' where creature_type = 'Fire Dragon';
+update Creature_Type set image = 'golddragonsmall.png' where creature_type = 'Gold Dragon';
+update Creature_Type set image = 'silverdragonsmall.png' where creature_type = 'Silver Dragon';

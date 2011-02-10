e31605ebaf6f8d4e5dc17a1c24c315c694f4d545
Sam Crawley <mutant.nz@gmail.com>
2011-02-10 21:03:49 +1300

    Added defence bonus for garrisons fighting in a building

--- lib/RPG/Combat/GarrisonBattle.pm
+++ lib/RPG/Combat/GarrisonBattle.pm
@@ -64,4 +64,26 @@ sub wipe_out_garrison {
    	$garrison->update;
 }
 
+after '_build_combat_factors' => sub {
+    my $self = shift;
+    
+    # Check if the garrison is in the same sector as a building. If so, add something to all their defence factors
+    my $building = $self->schema->resultset('Building')->find(
+        {
+            land_id => $self->location->id,
+        },
+        {
+            prefetch => 'building_type',
+        },
+    );
+    
+    # TODO: need to check the garrison is the owner? Or are they always the owner if they're in the sector?
+    
+    if ($building) {
+        foreach my $character ($self->garrsion->characters) {
+            $self->session->{combat_factors}{character}{ $character->id }{df} += $building->building_type->defense_factor;   
+        }   
+    }
+};
+
 1;

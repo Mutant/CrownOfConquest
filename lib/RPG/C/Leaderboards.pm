a996e640c9546369d988347dd3bf2cb3210425fb
Sam Crawley <mutant.nz@gmail.com>
2011-05-26 22:48:27 +1200

    Add paging to leaderboard. Refs #261

--- lib/RPG/C/Leaderboards.pm
+++ lib/RPG/C/Leaderboards.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use base 'Catalyst::Controller';
 
+use Math::Round qw(round);
+
 sub default : Path {
     my ( $self, $c ) = @_;
 
@@ -11,8 +13,23 @@ sub default : Path {
     
     my $sort = $c->req->param('sort') || 'xp_per_turn';
     
-    return unless grep {$_ eq $sort} @sort_options;    
+    return unless grep {$_ eq $sort} @sort_options;
+    
+    my $page = $c->req->param('page') || 1;    
 
+    my $party_count = $c->model('DBIC::Party')->search(
+        {
+            defunct => undef,
+            created => { '!=', undef },
+        },
+    )->count;
+    
+    my $page_count = $party_count / $c->config->{leaderboard_page_size};
+    $page_count = int $page_count + 1 if $page_count =~ /\./; 
+    $page = $page_count if $page > $page_count;
+    
+    my $offset = ($page - 1) * $c->config->{leaderboard_page_size};
+    
     my @parties = $c->model('DBIC::Party')->search(
         {
             defunct => undef,
@@ -23,9 +40,10 @@ sub default : Path {
             join      => 'characters',
             '+select' => [ \'sum(characters.xp) as total_xp', \'round(avg(characters.xp)) as average_xp', \'sum(characters.xp)/turns_used as xp_per_turn', \'count(character_id) as character_count' ],
             '+as'     => [ 'total_xp', 'average_xp', 'xp_per_turn', 'character_count' ],
-            order_by  => $sort . ' desc',
+            order_by  => $sort . ' desc, created',
             group_by  => 'me.party_id',
-            rows      => 20,
+            rows      => $c->config->{leaderboard_page_size},
+            offset    => $offset,
         },
     );
 
@@ -37,6 +55,9 @@ sub default : Path {
                 params   => { 
                     parties => \@parties,
                     current_sort => $sort,
+                    current_page => $page,
+                    page_count => $page_count,
+                    offset => $offset,
                 },
             }
         ]

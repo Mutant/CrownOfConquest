2f00d88d1e2e61a53cfd75d0505f333e888dc6cf
Sam Crawley <mutant.nz@gmail.com>
2011-10-31 19:26:59 +1300

    Tweaked levels. Refs #493

--- db/patch/0.7.8/changes01.sql
+++ db/patch/0.7.8/changes01.sql
@@ -4,3 +4,13 @@ ALTER TABLE `Player` ADD COLUMN `screen_width` VARCHAR(255)  NOT NULL DEFAULT 'a
 ALTER TABLE `Player_Login` ADD COLUMN `screen_height` INTEGER,
  ADD COLUMN `screen_width` INTEGER;
 
+update Levels set xp_needed = 3000 where level_number = 5;
+update Levels set xp_needed = 5000 where level_number = 6;
+update Levels set xp_needed = 7000 where level_number = 7;
+update Levels set xp_needed = 9200 where level_number = 8;
+update Levels set xp_needed = 11400 where level_number = 9;
+update Levels set xp_needed = 13600 where level_number = 10;
+update Levels set xp_needed = 16000 where level_number = 11;
+update Levels set xp_needed = 18500 where level_number = 12;
+update Levels set xp_needed = 21000 where level_number = 13;
+update Levels set xp_needed = 24000 where level_number = 14;

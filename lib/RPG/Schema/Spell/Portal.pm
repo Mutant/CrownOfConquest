732b4b4cbf980c1aa5bb9b1ecaed7f79980e4833
Sam Crawley <mutant.nz@gmail.com>
2011-12-04 12:55:04 +1300

    Can't become mayor if town is exited with Portal. Refs #546

--- lib/RPG/Schema/Spell/Portal.pm
+++ lib/RPG/Schema/Spell/Portal.pm
@@ -10,9 +10,21 @@ use Games::Dice::Advanced;
 sub _cast {
     my ( $self, $character, $target, $level ) = @_;
 
+    # Check if they're pending mayor, and clear it if they are
+    my $dungeon = $target->dungeon_grid->dungeon_room->dungeon;
+    
+    if ($dungeon->type eq 'castle') {
+        my $town = $dungeon->town;
+        
+        if ($town->pending_mayor == $target->id) {
+            $town->decline_mayoralty;
+            $town->update;
+        }
+    }
+
     $target->dungeon_grid_id(undef);
     $target->update;
-
+    
     return {
         type   => 'portal',
     };
@@ -22,7 +34,7 @@ sub can_cast {
 	my $self = shift;
 	my $character = shift;
 	
-	return $character->party->dungeon_grid_id ? 1 : 0;	
+	return $character->party->dungeon_grid_id ? 1 : 0;
 }
 
 1;

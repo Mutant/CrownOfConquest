00b61f2014a0fed492b20a68186273217dd566bb
Sam Crawley <mutant.nz@gmail.com>
2012-01-30 19:58:25 +1300

    Made check for inactive kingdoms more frequent. Refs #613

--- lib/RPG/NewDay/Action/Kingdom.pm
+++ lib/RPG/NewDay/Action/Kingdom.pm
@@ -27,9 +27,7 @@ sub run {
         }
     );
 
-    foreach my $kingdom (@kingdoms) {
-        next if $self->check_for_inactive($kingdom);
-        
+    foreach my $kingdom (@kingdoms) {        
         my $king = $kingdom->king;
         
         next unless $king;
@@ -273,77 +271,6 @@ sub cancel_quests_awaiting_acceptance {
         $quest->update;
         
     }       
-}
-
-# Mark any kingdoms with 0 towns as inactive.
-#  All land becomes neutral, King removed, and party become free citizens
-sub check_for_inactive {
-    my $self = shift;
-    my $kingdom = shift;
-    
-    my $c = $self->context;
-    
-    my $town_count = $c->schema->resultset('Town')->search(
-        {
-            'location.kingdom_id' => $kingdom->id
-        },
-        {
-            'join' => 'location',
-        }
-    )->count;
-    
-    return 0 if $town_count > 0;
-  
-    $kingdom->active(0);
-    $kingdom->fall_day_id($c->current_day->day_id);
-    $kingdom->update;
-    
-    $kingdom->search_related('sectors')->update( { kingdom_id => undef } );
-    $kingdom->town_loyalty->delete;
-    
-    my $king = $kingdom->king;
-    
-    # Make sure we include defunct parties
-    my @parties = $c->schema->resultset('Party')->search(
-        {
-            kingdom_id => $kingdom->id,
-        }
-    );
-
-    foreach my $party (@parties) {
-        $party->change_allegiance(undef);
-        $party->last_allegiance_change(undef);
-        $party->cancel_kingdom_quests($kingdom);
-        
-        if ($king->is_npc || $party->id != $king->party_id) {
-            $party->add_to_messages(
-                {
-                    day_id => $c->current_day->id,
-                    alert_party => 1,
-                    message => "The Kingdom of " . $kingdom->name . " has fallen. We are now free citizens",
-                }
-            );
-        }
-        
-        $party->update;
-    }
-
-    $king->status(undef);
-    $king->status_context(undef);
-    $king->update;
-    
-    if (! $king->is_npc) {
-        my $party = $king->party;
-        $party->add_to_messages(
-            {
-                day_id => $c->current_day->id,
-                alert_party => 1,
-                message => "Our mighty Kingdom of " . $kingdom->name . " has fallen, as we no longer own any towns. A sad day indeed.",
-            },
-        );
-    }
-   
-    return 1;
 }
 
 # Adjust loyalty of parties

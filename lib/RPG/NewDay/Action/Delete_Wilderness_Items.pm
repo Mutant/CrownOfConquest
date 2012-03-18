44055f56b029066db96b64f5117c50339fd71273
Sam Crawley <mutant.nz@gmail.com>
2012-03-18 17:25:42 +1300

    Randomly delete items in the wilderness. Refs #169

---
+++ lib/RPG/NewDay/Action/Delete_Wilderness_Items.pm
@@ -0,0 +1,27 @@
+package RPG::NewDay::Action::Delete_Wilderness_Items;
+
+use Moose;
+
+extends 'RPG::NewDay::Base';
+
+use Games::Dice::Advanced;
+
+sub run {
+    my $self = shift;
+    
+    my @items = $self->context->schema->resultset('Items')->search(
+        {
+            land_id => {'!=', undef},
+        }
+    );
+    
+    my $count = 0;
+    foreach my $item (@items) {
+        if (Games::Dice::Advanced->roll('1d100') < 3) {
+            $item->delete;
+            $count++;
+        }   
+    }
+    
+    $self->context->logger->debug("Deleted $count of " . scalar @items . " wilderness items");
+}

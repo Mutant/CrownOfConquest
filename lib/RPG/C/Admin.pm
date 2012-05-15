db85eae30e996ea0be8f797a7be11acc337c46b4
Sam Crawley <mutant.nz@gmail.com>
2012-05-15 20:40:31 +1200

    Added a 'login as' feature.

--- lib/RPG/C/Admin.pm
+++ lib/RPG/C/Admin.pm
@@ -16,4 +16,42 @@ sub default : Path {
 	$c->forward('/admin/items/edit_item_type');
 }
 
+sub login_as : Local {
+	my ($self, $c) = @_;
+	
+	if ($c->req->param('submit')) {
+	    my %args;
+	    if ($c->req->param('email')) {
+	        $args{email} = $c->req->param('email');
+	    }
+	    elsif ($c->req->param('player_name')) {
+	        $args{player_name} = $c->req->param('player_name');
+	    }
+	    else {
+	        return;
+	    }	    
+	    
+	    my $player = $c->model('DBIC::Player')->find(
+	       {
+	           %args,
+	       }
+	    ); 
+	    
+        $c->session->{player} = $player;
+        $c->res->redirect( $c->config->{url_root} );
+        return;
+	}
+	
+    $c->forward(
+        'RPG::V::TT',
+        [
+            {
+                template => 'admin/login_as.html',
+                params   => {},
+            }
+        ]
+    );	
+    
+}
+
 1;

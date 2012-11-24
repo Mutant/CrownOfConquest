a3ad4cdcaa24cf7fbd4fb773d4717eeb8337673c
Sam Crawley <mutant.nz@gmail.com>
2012-11-24 16:09:16 +1300

    Added another test for Garrison land claims. Refs #866

--- t/lib/Test/RPG/NewDay/Garrison.pm
+++ t/lib/Test/RPG/NewDay/Garrison.pm
@@ -77,5 +77,35 @@ sub test_garrison_unclaims_land : Tests(27) {
 	}
 }
 
+sub test_garrison_reclaims_land_when_party_changes_allegiance : Tests(27) {
+    my $self = shift;
+    
+    # GIVEN
+	my @land = Test::RPG::Builder::Land->build_land( $self->{schema} );
+	my $kingdom1 = Test::RPG::Builder::Kingdom->build_kingdom( $self->{schema} );
+	my $kingdom2 = Test::RPG::Builder::Kingdom->build_kingdom( $self->{schema} );
+	my $party = Test::RPG::Builder::Party->build_party( $self->{schema}, character_count => 2, land_id => $land[1]->id, kingdom_id => $kingdom1->id );
+	my $garrison = Test::RPG::Builder::Garrison->build_garrison( $self->{schema}, party_id => $party->id, land_id => $land[4]->id, 
+	   established => DateTime->now->subtract( days => 3 ), );
+	   
+    $garrison->claim_land;
+	   
+	$garrison->claim_land_order(1);
+	$garrison->update;
+	
+	# WHEN
+	$party->change_allegiance($kingdom2);
+	$party->update;
+	$self->{action}->run();
+	
+	# THEN
+	foreach my $land (@land) {
+	   $land->discard_changes;	
+	   is($land->kingdom_id, $kingdom2->id, "Land now claimed by new kingdom");    
+	   is($land->claimed_by_id, $garrison->id, "Garrison claimed sector " . $land->id);
+	   is($land->claimed_by_type, 'garrison', "Claimed by type correct for sector " . $land->id);     
+	}
+}
+
 
 1;

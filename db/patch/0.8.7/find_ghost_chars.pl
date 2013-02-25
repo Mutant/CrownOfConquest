1b66bfb1b272103083156912449b12080defa6e0
Sam Crawley <mutant.nz@gmail.com>
2013-02-25 20:12:30 +1300

    Clean up some bad data. Refs #914
    Must have been due to an old bug where characters weren't removed from
    town garrisons.

---
+++ db/patch/0.8.7/find_ghost_chars.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use RPG::Schema;
+use RPG::LoadConf;
+my $config = RPG::LoadConf->load();
+
+use Try::Tiny;
+
+my $schema = RPG::Schema->connect( $config, @{$config->{'Model::DBIC'}{connect_info}} );
+
+my @towns = $schema->resultset('Town')->search();
+
+foreach my $town (@towns) {
+    my $mayor = $town->mayor;
+    
+    #next if ! $mayor || $mayor->is_npc;
+    
+    my @garrison_chars = $schema->resultset('Character')->search(
+        {
+            status => 'mayor_garrison',
+            status_context => $town->id,
+        }
+    );
+    
+    foreach my $char (@garrison_chars) {
+        if (! $mayor || $char->party_id != $mayor->party_id) {
+            warn "Ghost char " . $char->id . " found in " . $town->id;
+            warn "from party.. " . $char->party->id;
+            $char->status(undef);
+            $char->status_context(undef);
+            $char->update;
+        }
+               
+    }
+}

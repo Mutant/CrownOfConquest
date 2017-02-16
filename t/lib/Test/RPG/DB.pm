9fd67d76c9605a9147329cf1707afef6ea1f2371
Sam Crawley <mutant.nz@gmail.com>
2017-02-17 09:18:12 +1300

    Removed hardcoded reference to test DB.

--- t/lib/Test/RPG/DB.pm
+++ t/lib/Test/RPG/DB.pm
@@ -14,17 +14,12 @@ sub db_startup : Test(startup) {
 
     return if $ENV{TEST_NO_DB};
 
-	my $schema;
-	if (defined $self->{config}->{'TestModel::DBIC'}) {
-		$schema = RPG::Schema->connect( $self->{config}, @{$self->{config}->{'TestModel::DBIC'}{connect_info}}, );
-	} else {
-   		$schema = RPG::Schema->connect( $self->{config}, "dbi:mysql:game_test", "root", "", { AutoCommit => 0 }, );
-	}
+	my $schema = RPG::Schema->connect( $self->{config}, @{$self->{config}->{'TestModel::DBIC'}{connect_info}}, );
 
     # Wrap in T::M::E so we can mock the config
     $schema = Test::MockObject::Extends->new($schema);
     $schema->fake_module( 'RPG::Schema', 'config' => sub { $self->{config} }, 'log' => sub { $self->{mock_logger} } );
-    
+
     #$schema->storage->dbh->begin_work;
 
     $self->{schema} = $schema;
@@ -38,32 +33,32 @@ sub aa_setup_context : Test(setup) {
     $self->{c}->mock(
         model => sub {
             my $resultset = $_[1];
-            
+
             if ($resultset eq 'DBIC') {
                 my $mock_model = Test::MockObject->new();
                 $mock_model->set_always('schema', $self->{schema});
                 return $mock_model;
             }
-            
+
             $resultset =~ s/^DBIC:://;
-            
+
             return $self->{mock_resultset}{$resultset} if $self->{mock_resultset}{$resultset};
-            
+
             return $self->{schema}->resultset( $resultset );
         }
     );
-    
+
     $self->{stash}{today} = Test::RPG::Builder::Day->build_day($self->{schema})
         unless $self->{dont_create_today};
 }
 
 sub roll_back : Test(teardown) {
     my $self = shift;
-    
+
     if ($ENV{TEST_COMMIT}) {
         $self->{schema}->storage->dbh->commit;
     }
-    else {    
+    else {
         $self->{schema}->storage->dbh->rollback;
     }
 }
